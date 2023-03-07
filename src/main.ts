bf5d2dc996f142561c3f09d546c54b69207ab44b
Edycar Reyes <edycarreyes@gmail.com>
2023-03-07 00:52:30 -0600

    implemented the firebase service account and start the app for the first time

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import * as dotenv from "dotenv";
+import * as process from "process";
+
+dotenv.config({});
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(parseInt(process.env.PORT));
 }
+
 bootstrap();
--- src/modules/auth/strategies/firebase-auth.strategy.ts
+++ src/modules/auth/strategies/firebase-auth.strategy.ts
@@ -1,30 +1,30 @@
-import { PassportStrategy } from '@nestjs/passport';
-import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
-import { Strategy, ExtractJwt } from 'passport-firebase-jwt';
-import * as firebase from 'firebase-admin';
-import { DecodedIdToken } from 'firebase-admin/lib/auth';
-import * as dotenv from 'dotenv';
+import { PassportStrategy } from "@nestjs/passport";
+import { Injectable, Logger, UnauthorizedException } from "@nestjs/common";
+import { Strategy, ExtractJwt } from "passport-firebase-jwt";
+import * as firebase from "firebase-admin";
+import { DecodedIdToken } from "firebase-admin/lib/auth";
+import * as dotenv from "dotenv";
 
 dotenv.config();
 
 const firebase_params = {
-  type: process.env.NOTELINK_FIREBASE_TYPE,
-  projectId: process.env.NOTELINK_FIREBASE_PROJECT_ID,
-  privateKeyId: process.env.NOTELINK_FIREBASE_PRIVATE_KEY_ID,
-  privateKey: process.env.NOTELINK_FIREBASE_PRIVATE_KEY,
-  clientEmail: process.env.NOTELINK_FIREBASE_CLIENT_EMAIL,
-  clientId: process.env.NOTELINK_FIREBASE_CLIENT_ID,
-  authUri: process.env.NOTELINK_FIREBASE_AUTH_URI,
-  tokenUri: process.env.NOTELINK_FIREBASE_TOKEN_URI,
+  type: process.env.QUANTUM_GIT_FIREBASE_TYPE,
+  projectId: process.env.QUANTUM_GIT_FIREBASE_PROJECT_ID,
+  privateKeyId: process.env.QUANTUM_GIT_FIREBASE_PRIVATE_KEY_ID,
+  privateKey: process.env.QUANTUM_GIT_FIREBASE_PRIVATE_KEY,
+  clientEmail: process.env.QUANTUM_GIT_FIREBASE_CLIENT_EMAIL,
+  clientId: process.env.QUANTUM_GIT_FIREBASE_CLIENT_ID,
+  authUri: process.env.QUANTUM_GIT_FIREBASE_AUTH_URI,
+  tokenUri: process.env.QUANTUM_GIT_FIREBASE_TOKEN_URI,
   authProviderX509CertUrl:
-    process.env.NOTELINK_FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-  clientC509CertUrl: process.env.NOTELINK_FIREBASE_CLIENT_X509_CERT_URL,
+    process.env.QUANTUM_GIT_FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  clientC509CertUrl: process.env.QUANTUM_GIT_FIREBASE_CLIENT_X509_CERT_URL,
 };
 
 @Injectable()
 export class FirebaseAuthStrategy extends PassportStrategy(
   Strategy,
-  'firebase-jwt',
+  "firebase-jwt",
 ) {
   private defaultApp: firebase.app.App;
   private logger: Logger = new Logger(this.name);
