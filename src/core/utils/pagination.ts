56fb49e3c16c3d863b5dcaccbc04a9ef9689389a
Edycar Reyes <edycarreyes@gmail.com>
2023-03-07 17:42:39 -0600

    generated the utils file to parse the pagination string incoming from the octokit library to a pagination object

---
+++ src/core/utils/pagination.ts
@@ -0,0 +1,27 @@
+import { IPagination } from "../interfaces/pagination";
+
+export function parsePaginationString(paginationString: string): IPagination {
+  const pagination: IPagination = {
+    first: null,
+    previous: null,
+    next: null,
+    last: null,
+  };
+  const paginationArray = this.paginationString.match(/<([^;]*)>; rel="([^;]*)"/g);
+
+  paginationArray?.forEach((link: string) => {
+    const urlParams = new URLSearchParams(link.match(/<([^;]*)>/g)?.at(0));
+    const page = parseInt(urlParams.get("page") ?? "0", 10);
+    if (link.includes("prev")) {
+      pagination.previous = page;
+    } else if (link.includes("first")) {
+      pagination.first = page;
+    } else if (link.includes("next")) {
+      pagination.next = page;
+    } else if (link.includes("last")) {
+      pagination.last = page;
+    }
+  });
+
+  return pagination;
+}
