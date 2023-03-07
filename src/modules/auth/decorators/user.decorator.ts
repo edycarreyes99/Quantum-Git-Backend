4fa76f2859201a511b58021da437d1b1970a9e1b
Edycar Reyes <edycarreyes@gmail.com>
2023-03-06 23:56:37 -0600

    generated the logic to extract que http user requestor that is going to be used in the http requests.

--- src/modules/auth/decorators/user.decorator.ts
+++ src/modules/auth/decorators/user.decorator.ts
@@ -1,3 +1,6 @@
-import { SetMetadata } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const User = (...args: string[]) => SetMetadata('user', args);
+export const User = createParamDecorator((data: any, ctx: ExecutionContext) => {
+  const request = ctx.switchToHttp().getRequest();
+  return request.user;
+});
