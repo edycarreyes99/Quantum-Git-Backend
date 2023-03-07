8e339e133f9d332937c72be7e8c3c14c7cb691eb
Edycar Reyes <edycarreyes@gmail.com>
2023-03-07 00:08:13 -0600

    injected the firebase auth strategy in the auth module and the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    PassportModule.register({ defaultStrategy: 'firebase-jwt' }),
+  ],
   controllers: [AppController],
+  exports: [PassportModule],
   providers: [AppService],
 })
 export class AppModule {}
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './services/auth.service';
 import { AuthController } from './controllers/auth.controller';
+import { FirebaseAuthStrategy } from './strategies/firebase-auth.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, FirebaseAuthStrategy],
 })
 export class AuthModule {}
