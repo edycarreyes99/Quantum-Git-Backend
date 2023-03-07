c721f84ee09a702cbf3a06ccad090b78d54b597a
Edycar Reyes <edycarreyes@gmail.com>
2023-03-07 15:40:15 -0600

    refactored the user decorator from '@User()'->'@AuthenticatedUser()'.

--- src/modules/auth/decorators/authenticated-user.decorator.ts
+++ src/modules/auth/decorators/authenticated-user.decorator.ts
@@ -1,6 +1,6 @@
 import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 
-export const User = createParamDecorator((data: any, ctx: ExecutionContext) => {
+export const AuthenticatedUser = createParamDecorator((data: any, ctx: ExecutionContext) => {
   const request = ctx.switchToHttp().getRequest();
   request.user.github_access_token = request.headers.secondary_authorization
   return request.user;
--- src/modules/repos/controllers/repos.controller.ts
+++ src/modules/repos/controllers/repos.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Param, UseGuards } from "@nestjs/common";
 import { ReposService } from "../services/repos.service";
-import { User } from "../../auth/decorators/user.decorator";
+import { AuthenticatedUser } from "../../auth/decorators/authenticated-user.decorator";
 import { AuthenticatedGuard } from "../../auth/guards/authenticated/authenticated.guard";
 import { IAuthenticatedUser } from "../../auth/interfaces/authenticated-user.interface";
 
@@ -12,12 +12,12 @@ export class ReposController {
 
 
   @Get()
-  findAll(@User() user: IAuthenticatedUser) {
+  findAll(@AuthenticatedUser() user: IAuthenticatedUser) {
     return this.reposService.findAll(user);
   }
 
   @Get(":repoId")
-  findOne(@Param("repoId") repoId: string, @User() user: IAuthenticatedUser) {
+  findOne(@AuthenticatedUser() user: IAuthenticatedUser, @Param("repoId") repoId: string) {
     return this.reposService.findOne(+repoId);
   }
 }
