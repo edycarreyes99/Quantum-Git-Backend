fa1bfa676fdc9c0f1585f1a260e131191e5caf04
Edycar Reyes <edycarreyes@gmail.com>
2023-03-07 17:43:15 -0600

    generated the logic for the Octokit service

--- src/core/services/github/octokit.service.ts
+++ src/core/services/github/octokit.service.ts
@@ -1,11 +1,40 @@
 import { Octokit } from "octokit";
+import { IPaginatedResponse } from "../../interfaces/paginated-response.interface";
+import { Logger } from "@nestjs/common";
+import { OctokitResponse } from "@octokit/types/dist-types/OctokitResponse";
+import { parsePaginationString } from "../../utils/pagination";
 
-export abstract class GitHubService<T> {
+export abstract class OctokitService<T> {
+  private readonly logger = new Logger(OctokitService.name);
   private octokit: Octokit;
-  private githubAccessToken: string = "";
 
-  constructor() {
+  // Method to initialize the octokit object
+  private initialize(githubAccessToken: string): Promise<void> {
+    return new Promise<void>(async (resolve, rejects) => {
+      this.octokit = new Octokit({
+        auth: githubAccessToken,
+      });
+      resolve();
+    });
+  }
 
+  // Method to make a request=
+  public request(githubAccessToken: string, requestUrl: string, params: Record<string, any>): Promise<IPaginatedResponse<T>> {
+    return new Promise<IPaginatedResponse<T>>(async (resolve, rejects) => {
+      await this.initialize(githubAccessToken).then(async () => {
+        await this.octokit.request(requestUrl, params).then((response: OctokitResponse<T[]>) => {
+          const paginatedResponse: IPaginatedResponse<T> = {
+            ...parsePaginationString(response.headers.link),
+            data: response.data,
+          };
+
+          resolve(paginatedResponse);
+        }).catch((error) => {
+          this.logger.error(`Error executing github request: ${error}`);
+          rejects(error);
+        });
+      });
+    });
   }
 
 }
