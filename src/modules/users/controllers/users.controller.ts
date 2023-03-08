f0f4015296a894a19b52765317ba97d757dc6d39
Edycar Reyes <edycarreyes@gmail.com>
2023-03-08 01:40:05 -0600

    implemented the logic to return the current authenticated GitHub user.

--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -1,8 +1,11 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, UseGuards } from "@nestjs/common";
 import { UsersService } from "../services/users.service";
 import { ApiBearerAuth, ApiHeader, ApiTags } from "@nestjs/swagger";
+import { AuthenticatedUser } from "../../auth/decorators/authenticated-user.decorator";
+import { IUser } from "../interfaces/user.interface";
+import { AuthenticatedGuard } from "../../auth/guards/authenticated/authenticated.guard";
 
-@ApiTags('Users')
+@ApiTags("Users")
 @ApiBearerAuth()
 @ApiHeader({
   name: "Secondary_Authorization",
@@ -10,7 +13,13 @@ import { ApiBearerAuth, ApiHeader, ApiTags } from "@nestjs/swagger";
   required: true,
 })
 @Controller("users")
+@UseGuards(AuthenticatedGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {
+  }
+
+  @Get("")
+  findCurrentAuthenticatedGitHubUser(@AuthenticatedUser() user): Promise<IUser> {
+    return this.usersService.findCurrentAuthenticatedGitHubUser(user);
   }
 }
--- src/modules/users/services/users.service.ts
+++ src/modules/users/services/users.service.ts
@@ -1,5 +1,17 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { IAuthenticatedUser } from "../../auth/interfaces/authenticated-user.interface";
+import { IUser } from "../interfaces/user.interface";
 
 @Injectable()
 export class UsersService {
+
+  // Method to return the current authenticated GitHub user
+  findCurrentAuthenticatedGitHubUser(authenticatedUser: IAuthenticatedUser): Promise<IUser> {
+    return new Promise<IUser>(async (resolve, rejects) => {
+      if (authenticatedUser?.github_user) {
+        resolve(authenticatedUser.github_user);
+      }
+      rejects(new UnauthorizedException("No GitHub user authenticated found."));
+    });
+  }
 }
